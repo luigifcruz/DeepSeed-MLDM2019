1d9da54a75b583773d6920071236fb0ab236ca2d
Luigi Freitas Cruz <luigifcruz@icloud.com>
2017-10-31 00:40:36 -0200

    Graph Improvements

--- TrainCNN.py
+++ TrainCNN.py
@@ -6,8 +6,8 @@ import sys
 batch_size = 10
 test_size = 40
 keep_rate = 0.8
-n_iteration = 2000
-dense_size = 512
+n_iteration = 1
+dense_size = 1024
 learning_rate = 1e-4 #AdamOptimizer
 
 # Dataset Features
@@ -43,17 +43,19 @@ def get_single_features(filename_queue):
 
     return image, label
 
-def get_shuffle_batch(tfrecords_filename, batch_size):
-    filename_queue = tf.train.string_input_producer([tfrecords_filename])
-    image, label = get_single_features(filename_queue)
+def get_shuffle_batch(tfrecords_filename, batch_size, name):
+    with tf.name_scope(name):
+        filename_queue = tf.train.string_input_producer([tfrecords_filename])
+        image, label = get_single_features(filename_queue)
 
-    return tf.train.shuffle_batch([image, label], batch_size=batch_size, num_threads=2, capacity=600, min_after_dequeue=400)
+        return tf.train.shuffle_batch([image, label], batch_size=batch_size, num_threads=2, capacity=600, min_after_dequeue=400)
 
-def get_batch(tfrecords_filename, batch_size):
-    filename_queue = tf.train.string_input_producer([tfrecords_filename])
-    image, label = get_single_features(filename_queue)
+def get_batch(tfrecords_filename, batch_size, name):
+    with tf.name_scope(name):
+        filename_queue = tf.train.string_input_producer([tfrecords_filename])
+        image, label = get_single_features(filename_queue)
 
-    return tf.train.batch([image, label], batch_size=batch_size, allow_smaller_final_batch=True)
+        return tf.train.batch([image, label], batch_size=batch_size, allow_smaller_final_batch=True)
 
 def convolutional_layer(input, size_in, size_out, name):
     with tf.name_scope(name):
@@ -67,30 +69,31 @@ def convolutional_layer(input, size_in, size_out, name):
         return tf.nn.max_pool(act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
 def convolutional_neural_network(x):
-    # Reshape Image
-    input_layer = tf.reshape(x, shape=[-1, image_height, image_width, n_channels])
-    image_summary = tf.summary.image("Example_Images", input_layer)
+    with tf.name_scope("CNN"):
+        # Reshape Image
+        input_layer = tf.reshape(x, shape=[-1, image_height, image_width, n_channels])
+        image_summary = tf.summary.image("Example_Images", input_layer)
 
-    # Convolution Layer
-    convolution = convolutional_layer(input_layer, n_channels, 32, "Conv_1")
-    convolution = convolutional_layer(convolution, 32, 64, "Conv_2")
-    convolution = convolutional_layer(convolution, 64, 128, "Conv_3")
-    convolution = convolutional_layer(convolution, 128, 256, "Conv_4")
+        # Convolution Layer
+        convolution = convolutional_layer(input_layer, n_channels, 32, "Conv_1")
+        convolution = convolutional_layer(convolution, 32, 64, "Conv_2")
+        convolution = convolutional_layer(convolution, 64, 128, "Conv_3")
+        convolution = convolutional_layer(convolution, 128, 256, "Conv_4")
 
-    # Flatten Layer
-    input_size = int(image_width/16)*int(image_height/16)*256
-    flattened = tf.reshape(convolution, [-1, input_size])
+        # Flatten Layer
+        input_size = int(image_width/16)*int(image_height/16)*256
+        flattened = tf.reshape(convolution, [-1, input_size])
 
-    # Dense Layer
-    dense = tf.layers.dense(inputs=flattened, units=dense_size, activation=tf.nn.relu, name="Dense_Layer")
+        # Dense Layer
+        dense = tf.layers.dense(inputs=flattened, units=dense_size, activation=tf.nn.relu, name="Dense_Layer")
 
-    # Dropout Layer
-    dropout = tf.layers.dropout(inputs=dense, rate=keep_prob, name="Dropout_Layer")
+        # Dropout Layer
+        dropout = tf.layers.dropout(inputs=dense, rate=keep_prob, name="Dropout_Layer")
 
-    # Logits Layer
-    logits = tf.layers.dense(inputs=dropout, units=n_classes, name="Logits_Layer")
+        # Logits Layer
+        logits = tf.layers.dense(inputs=dropout, units=n_classes, name="Logits_Layer")
 
-    return logits, dense
+        return logits, dense
 
 def train_neural_network(x):
     prediction, dense_out  = convolutional_neural_network(x)
@@ -104,9 +107,9 @@ def train_neural_network(x):
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
         tf.summary.scalar('Accuracy', accuracy)
 
-    train_images, train_labels = get_shuffle_batch('train-dataset.tfrecords', batch_size)
-    valid_images, valid_labels = get_shuffle_batch('valid-dataset.tfrecords', batch_size)
-    test_images, test_labels = get_batch('test-dataset.tfrecords', test_size)
+    train_images, train_labels = get_shuffle_batch('train-dataset.tfrecords', batch_size, 'Train_Dataset')
+    valid_images, valid_labels = get_shuffle_batch('valid-dataset.tfrecords', batch_size, 'Valid_Dataset')
+    test_images, test_labels = get_batch('test-dataset.tfrecords', test_size, 'Test_Dataset')
     train_num_examples = get_size('train-dataset.tfrecords')
 
     embedding = tf.Variable(tf.zeros([test_size, dense_size]), name="test_embedding")
@@ -120,8 +123,9 @@ def train_neural_network(x):
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
         merged = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter(LOGDIR + "train", sess.graph)
+        train_writer = tf.summary.FileWriter(LOGDIR + "train")
         valid_writer = tf.summary.FileWriter(LOGDIR + "validation")
+        train_writer.add_graph(sess.graph)
 
         config = tf.contrib.tensorboard.plugins.projector.ProjectorConfig()
         embedding_config = config.embeddings.add()
@@ -143,7 +147,7 @@ def train_neural_network(x):
             train_writer.add_summary(summary, i)
             train_writer.flush()
 
-            if i % 50 == 0:
+            if i % 100 == 0:
                 print("Calculating and saving assignments...")
                 tbatch_xs, tbatch_ys = sess.run([test_images, test_labels])
                 sess.run(assignment, feed_dict={x: tbatch_xs, y: tbatch_ys, keep_prob: 1.0})
